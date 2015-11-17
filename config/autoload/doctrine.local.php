72c4f335d209ca6978ba8ac3dfdbbc113ae2dcba
lyh <286067723@qq.com>
2015-11-17 10:35:27 +0800

    1

--- config/autoload/doctrine.local.php
+++ config/autoload/doctrine.local.php
@@ -5,15 +5,14 @@ return array(
             'orm_default' => array(
                 'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
                 'params' => array(
-                    'host'     => '127.0.0.1',
+                    'host'     => 'localhost',
                     'port'     => '3306',
                     'user'     => 'root',
-                    'password' => '',
-                    'dbname'   => 'dhtvweixin',
+                    'password' => 'root',
+                    'dbname'   => 'zf2tutorial',
                     'driverOptions' => array(
                         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
                     ),
-                    'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock'
                 )
             )
         )
